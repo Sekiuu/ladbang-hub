5607e120eae8da3861b8a7898723003fd4be0a9a
Sekiuu <sega.pamma@gmail.com>
2025-10-07 23:52:48 +0700

    update : readme and package

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 Hello it's my PSCP project
+
+Before start project make sure your create .venv in this directory
+
+- to create .venv you can run this command in terminal : python -m venv .venv
+- affter created .venv you must create file .gitignore -> .venv/.gitignore
+
+Then use this comman to install packages for python : pip install -r requirements.txt
+
+To Run Web-app correctly you have to run both "server" and "client"
+\*\* To run client, run this command in terminal
+
+- cd client
+- npm i
+- npm run dev
+
+\*\* To run server, run this command in terminal
+
+- cd server-fastApi
+- uvicorn main:app --reload
--- client/app/landing/home.tsx
+++ client/app/landing/home.tsx
@@ -3,30 +3,50 @@ import React from "react";
 import type { ResponseData } from "../api";
 import ButtonUI from "../compoents/ui/Button";
 import { useState } from "react";
-export default function HomePage({ testData, test2Data }: { testData: ResponseData | null, test2Data: ResponseData | null }) {
-    const [response, setResponseData] = useState<ResponseData | null>(null);
-    const [body,setBody] = useState<string>("");
-    const handleTestAPI = (num: number) => {
-        if (num == 0) {
-            setResponseData(testData);
-            console.log(testData)
-            setBody(JSON.stringify(testData?.body))
-        }
-        else {
-            setResponseData(test2Data)
-            console.log(test2Data)
-            setBody(JSON.stringify(test2Data?.body))
-        }
+export default function HomePage({
+  testData,
+  test2Data,
+}: {
+  testData: ResponseData | null;
+  test2Data: ResponseData | null;
+}) {
+  const [response, setResponseData] = useState<ResponseData | null>(null);
+  const [body, setBody] = useState<string>("");
+  const handleTestAPI = (num: number) => {
+    if (num == 0) {
+      setResponseData(testData);
+      console.log(testData);
+      setBody(JSON.stringify(testData?.body));
+    } else {
+      setResponseData(test2Data);
+      console.log(test2Data);
+      setBody(JSON.stringify(test2Data?.body));
     }
-    return (
-        <div className="col-span-4 bg-red-500 w-full gap-5 h-full flex flex-col items-center justify-center">
-            <h1 className="text-2xl font-bold">Test</h1>
-            <div className="bg-white rounded-xl p-5 min-w-md min-h-20">
-                <p className="text-lg">{response ? response.message : ""}</p>
-                <p className="text-lg">{body}</p>
-            </div>
-            <ButtonUI onClick={() => { handleTestAPI(0) }} className="min-w-lg">Test API</ButtonUI>
-            <ButtonUI onClick={() => { handleTestAPI(1) }} className="min-w-lg">Test API 2</ButtonUI>
-        </div>
-    );
+  };
+
+  return (
+    <div className="col-span-4 bg-red-500 w-full gap-5 h-full flex flex-col items-center justify-center">
+      <h1 className="text-2xl font-bold">Test</h1>
+      <div className="bg-white rounded-xl p-5 min-w-md min-h-20">
+        <p className="text-lg">{response ? response.message : ""}</p>
+        <p className="text-lg">{body}</p>
+      </div>
+      <ButtonUI
+        onClick={() => {
+          handleTestAPI(0);
+        }}
+        className="min-w-lg"
+      >
+        Test API
+      </ButtonUI>
+      <ButtonUI
+        onClick={() => {
+          handleTestAPI(1);
+        }}
+        className="min-w-lg"
+      >
+        Test API 2
+      </ButtonUI>
+    </div>
+  );
 }
