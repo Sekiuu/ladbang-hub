98264279b174b70847af943881e4c002459f4eaa
GuoyJeng <guojing2549@gmail.com>
2025-10-14 00:11:33 +0700

    Add google-genai

---
+++ server-fastApi/ai.py
@@ -0,0 +1,16 @@
+from google import genai
+from google.genai import types
+import os
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Explain how AI works in a few words",
+    config=types.GenerateContentConfig(
+        thinking_config=types.ThinkingConfig(thinking_budget=0) # Disables thinking
+    ),
+)
+print(response.text)
