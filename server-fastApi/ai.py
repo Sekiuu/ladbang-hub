74e6c25748c9053392a35dd6072b47b3549ce570
GuoyJeng <guojing2549@gmail.com>
2025-10-14 23:30:59 +0700

    Update ai.py

--- server-fastApi/ai.py
+++ server-fastApi/ai.py
@@ -1,22 +1,26 @@
-def routeAi():
-    from google import genai
-    from google.genai import types
-    import os
-    import fastapi
-    
-    app = fastapi.FastAPI()
-    
-    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+from google import genai
+from google.genai import types
+import os
+import fastapi
 
-    client = genai.Client(api_key=GEMINI_API_KEY)
+ai_router = fastapi.FastAPI()
 
-    app.get("/ai-promt")
-    def ai_promt():
-        response = client.models.generate_content(
-            model="gemini-2.5-flash",
-            contents="Explain how AI works in a few words",
-            config=types.GenerateContentConfig(
-                thinking_config=types.ThinkingConfig(thinking_budget=0) # Disables thinking
-            ),
-        )
-        print(response.text)
+from dotenv import load_dotenv
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+print(GEMINI_API_KEY)
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+ai_router.get("/ai-promt")
+def ai_promt(promt: str):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=promt,
+        config=types.GenerateContentConfig(
+            thinking_config=types.ThinkingConfig(thinking_budget=0) # Disables thinking
+        ),
+    )
+    print(response.text)
+    return {"response": response.text}
