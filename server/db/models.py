7f7658d8f9b469be63b25f75a89a41a0f8b0af61
GuoyJeng <guojing2549@gmail.com>
2025-11-02 23:40:48 +0700

    Add records table

--- server/db/models.py
+++ server/db/models.py
@@ -11,3 +11,16 @@ class User(Model):
     class Meta:
         table = "users"
         indexes = ("email",)
+
+class Record(Model):
+    id = fields.IntField(pk=True)
+    user_id = fields.ForeignKeyField("models.User", description="user_id")
+    types = fields.CharField(max_length=1000, description="type of record")
+    amount = fields.FloatField(description="amount")
+    detail = fields.CharField(max_length=10000, description="detail")
+    created_at = fields.DatetimeField(auto_now_add=True)
+    last_updated = fields.DatetimeField(auto_now=True)
+
+    class Meta:
+        table = "records"
+        indexes = ("user_id","created_at")
