48f190652cc6db8689607f0d4670641bc7004ec3
GuoyJeng <guojing2549@gmail.com>
2025-11-03 01:10:28 +0700

    Fix models.py

--- server/db/models.py
+++ server/db/models.py
@@ -6,7 +6,7 @@ class User(Model):
     username = fields.CharField(max_length=64, unique=True, description="username")
     email = fields.CharField(max_length=255, unique=True, description="email")
     password = fields.CharField(max_length=255, description="password_hash")
-    created_at = fields.DatetimeField()
+    created_at = fields.DatetimeField(auto_now_add=True, timestamptz=True)
 
     class Meta:
         table = "users"
@@ -14,13 +14,14 @@ class User(Model):
 
 class Record(Model):
     id = fields.IntField(pk=True)
-    user_id = fields.ForeignKeyField("models.User", description="user_id")
-    types = fields.CharField(max_length=1000, description="type of record")
-    amount = fields.FloatField(description="amount")
-    detail = fields.CharField(max_length=10000, description="detail")
-    created_at = fields.DatetimeField(auto_now_add=True)
-    last_updated = fields.DatetimeField(auto_now=True)
+    user = fields.ForeignKeyField("models.User", description="owner")
+    amout = fields.FloatField(description="amount")
+    type = fields.CharField(max_length=255, description="type of record")
+    detail = fields.CharField(max_length=10**4, description="detail")
+    tag = fields.CharField(max_length=255, description="tag")
+    last_edit = fields.DatetimeField(auto_now=True, timestamptz=True)
+    created_at = fields.DatetimeField(auto_now_add=True, timestamptz=True)
 
     class Meta:
         table = "records"
-        indexes = ("user_id","created_at")
+        indexes = ("user_id", "created_at")
