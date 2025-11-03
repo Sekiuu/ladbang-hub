2f279af13ea72e049912567b0f6b86a7e6bf6365
Vinniel <155176879+Vinniel5403@users.noreply.github.com>
2025-11-04 01:29:56 +0700

    add : middleware

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,10 +5,7 @@ import { redirect } from "next/navigation";
 // import LandingPage from "./landing/page";
 export default async function Home() {
   const session = await getServerSession(authOptions);
-
-  if (!session) {
-    return redirect("/landing");
-  }
+  
 
   return (
     <div className="grid justify-center items-center h-screen bg-white text-black">
