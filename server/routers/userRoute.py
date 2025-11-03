2dfc9dd71bbb1e22e6a1bb4b9e11f4b751fa38eb
Sekiuu <sega.pamma@gmail.com>
2025-11-03 14:41:03 +0700

    update : better err hadle in api users/verify

--- server/routers/userRoute.py
+++ server/routers/userRoute.py
@@ -112,8 +112,9 @@ async def verify_user(credentials: VerifyCredentials):
         }
     except Exception as e:
         logger.error(f"Error verifying user: {str(e)}")
-        return {
-            "message": "Server error",
-            "body": None,
-            "success": False,
-        }
+        raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")
+        # return {
+        #     "message": "Server error",
+        #     "body": None,
+        #     "success": False,
+        # }
